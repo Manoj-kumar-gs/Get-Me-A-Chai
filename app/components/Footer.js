cf2ac5f742392996158ab07ad504e89a5acf364b
Manoj-kumar-gs <manojappu315@gmail.com>
2025-05-29 18:16:32 +0530

    used link tags instead of anchor tags

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,4 +1,6 @@
+"use client"
 import React from 'react'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
@@ -6,8 +8,13 @@ const Footer = () => {
         <p>© Get Me A Chai - All rights reserved</p>
         <p className='flex justify-center items-center w-full gap-1.5'>
             Follow me on 
-            <a href="https://www.linkedin.com/in/manoj-kumar-gs-9333b22b8/" target="_blank" rel="noopener noreferrer"><img className='w-4 h-4' src="/linkedin.webp" alt="" /></a> <span className='font-bold flex justify-center items-start'>.</span> 
-            <a href="https://github.com/Manoj-kumar-gs" target="_blank" rel="noopener noreferrer"><img className='w-5 h-5 rounded-full' src="/github.webp" alt="" /></a>
+            <Link href="https://www.linkedin.com/in/manoj-kumar-gs-9333b22b8/" target="_blank" rel="noopener noreferrer">
+              <img className='w-4 h-4' src="/linkedin.webp" alt="" />
+            </Link> 
+            <span className='font-bold flex justify-center items-start'>.</span> 
+            <Link href="https://github.com/Manoj-kumar-gs" target="_blank" rel="noopener noreferrer">
+              <img className='w-5 h-5 rounded-full' src="/github.webp" alt="" />
+            </Link>
         </p>
     </footer>
   )
